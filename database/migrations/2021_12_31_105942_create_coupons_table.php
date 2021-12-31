7d969b4b86cf8b956a91e43b9fe19ab277b75619
unknown <marlon.paulo.silva@outlook.com>
2021-12-31 08:14:31 -0300

    Criando Cupons

---
+++ database/migrations/2021_12_31_105942_create_coupons_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->bigIncrements('coupon_id');
+            $table->string('coupon_code')->unique();
+            $table->integer('coupon_type');
+            $table->integer('coupon_value');
+            $table->integer('cart_min__value');
+            $table->dateTime('expired_on');
+            $table->integer('coupon_status');
+            $table->dateTime('added_on');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
