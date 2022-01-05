9104911d420a710e7b6b7e1ed1be2eee47bce054
unknown <marlon.paulo.silva@outlook.com>
2022-01-05 00:56:45 -0300

    adicionando secure_asset

--- resources/views/FrontEnd/master.blade.php
+++ resources/views/FrontEnd/master.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <!--Css Custom -->
-    <link rel="stylesheet" href="{{ asset('/') }}FrontEndSourceFile/css/style.css">
+    <link rel="stylesheet" href="{{ secure_asset('/') }}FrontEndSourceFile/css/style.css">
 </head>
   <body>
     <!-- Header -->
